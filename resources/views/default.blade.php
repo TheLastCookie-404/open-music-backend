d902c92f0b488fc538f540508fe378bc185134ca
Шарипов Елисей <e.sharipov@1lombard.local>
2025-11-06 16:56:42 +0500

    Devcontainer updated

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -2,25 +2,25 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+  <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.bunny.net">
+  <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-    <!-- Styles / Scripts -->
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @else
-        <style></style>
-    @endif
+  <!-- Styles / Scripts -->
+  @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  @else
+  <style></style>
+  @endif
 </head>
 
 <body>
-    <div class="fixed size-full grid place-items-center text-2xl">Nothing here yet!</div>
+  <div class="fixed size-full grid place-items-center text-2xl">Nothing here yet! </div>
 </body>
 
 </html>
