19a752147091d580370b651568f58ef32cbb942f
TheLastCookie-404 <se660745@gmail.com>
2025-11-11 10:29:21 +0500

    Auth done

--- routes/api.php
+++ routes/api.php
@@ -3,18 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::group(['middleware' => 'auth:sanctum'], function () {});
+Route::group(['middleware' => 'auth:sanctum'], function () {
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 
-Route::get('/test', function (Request $request) {
-    return 'Hello bebra!';
-})->middleware('auth:sanctum');
-
-Route::get('/test2', function (Request $request) {
-    return 'Hello bebra!';
+    Route::get('/test', function (Request $request) {
+        return 'Hello bebra!';
+    });
 });
 
 Auth::routes();
