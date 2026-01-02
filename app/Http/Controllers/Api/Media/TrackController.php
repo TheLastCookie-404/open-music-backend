2e18926b53a06c6fba5cd754b2231ad0ef611154
TheLastCookie-404 <se660745@gmail.com>
2026-01-02 22:07:37 +0500

    feature: simple search added

--- app/Http/Controllers/Api/Media/TrackController.php
+++ app/Http/Controllers/Api/Media/TrackController.php
@@ -4,15 +4,37 @@ namespace App\Http\Controllers\Api\Media;
 
 use App\Http\Controllers\Controller;
 use App\Models\Media;
+use Illuminate\Http\Request;
 
 class TrackController extends Controller
 {
     /**
      * Display the specified resource.
      */
-    public function show(Media $media) {
-        return response()->json([
-            "message" => $media->all()
+    public function show(Request $request, Media $media)
+    {
+        $request->validate([
+            'name' => 'nullable|string|max:218|min:1'
         ]);
+
+        $trackName = $request->get('name');
+
+        return response()->json(
+            $this->getTrack($media, $trackName)
+        );
+    }
+
+    private function getTrack(Media $media, ?string $trackName)
+    {
+        $trackList = $media::when($trackName, function ($query) use ($trackName) {
+            return $query->where('title', '=', $trackName);
+        })->latest('id')->get();
+
+        $tracksCount = count($trackList);
+
+        return [
+            'found' => $tracksCount,
+            'tracks' => $trackList
+        ];
     }
 }
