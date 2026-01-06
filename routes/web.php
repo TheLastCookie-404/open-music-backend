5dfa1d9722724f7625611344ad8cb53b17741305
TheLastCookie-404 <se660745@gmail.com>
2026-01-06 13:03:50 +0500

    feat: api root updated

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'open-music-api' => [
+            '    _____   _____  _______ __    _     _______ _     _ _______ _____ _______   ',
+            '   |     | |_____] |______ | \  | ___ |  |  | |     | |______   |   |          ',     
+            '   |_____| |       |______ |  \_|     |  |  | |_____| ______| __|__ |_____     '
+        ],
+        'version' => '1.0',
+        'documentation' => 'https://',
+        'author' => 'TheLastCookie-404',
+        'github' => 'https://github.com/TheLastCookie-404',
+        'message' => 'press autoformat to get normalized urls!'
+    ], 200, [], JSON_PRETTY_PRINT);
 });
