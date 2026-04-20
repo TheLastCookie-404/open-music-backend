072eac4d6cba52c9d231f5b7ad5ec4c31a5cc390
TheLastCookie-404 <se660745@gmail.com>
2026-04-20 12:05:28 +0500

    feat: seeder updated

--- app/Http/Controllers/Api/Auth/ProfileController.php
+++ app/Http/Controllers/Api/Auth/ProfileController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Api\AuthController;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Log;
 
 class ProfileController extends AuthController
 {
