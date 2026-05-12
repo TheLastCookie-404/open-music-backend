e61df384b384808f384fc509b462a0ca9f65e057
TheLastCookie-404 <se660745@gmail.com>
2026-05-12 19:19:51 +0500

    refactor: routing

--- routes/api.php
+++ routes/api.php
@@ -25,24 +25,70 @@ use App\Http\Controllers\Api\Playlist\
     UpdatePlaylistController
 };
 
-Route::middleware('auth:api')->group(function () {
-    Route::get('profile', [ProfileController::class, 'show']);
-    Route::post('logout', [LogoutController::class, 'index']);
-    Route::post('upload', [UploadController::class, 'store']);
-    Route::post('role', [UpdateRoleController::class, 'update']);
-    Route::post('tracks/delete', [DeleteController::class, 'destroy']);
+Route::prefix('/auth')->group(function () {
+    Route::post('register', [RegisterController::class, 'index']); // Registers new User
+    Route::post('login', [LoginController::class, 'index']); // Logs User in
+    Route::post('refresh', [RefreshController::class, 'index']); // Refreshes User auth token
 
-    Route::get('playlists', [PlaylistController::class, 'index']);
-    Route::get('playlist/track', [PlaylistController::class, 'show']);
-    Route::post('playlist', [CreatePlaylistController::class, 'store']);
-    Route::post('playlist/add', [UpdatePlaylistController::class, 'store']);
-    Route::post('playlist/remove', [UpdatePlaylistController::class, 'destroy']);
-    Route::post('playlist/delete', [DeletePlaylistController::class, 'destroy']);
+    Route::middleware('auth:api')->group(function () {
+        Route::get('profile', [ProfileController::class, 'show']); // Shows profile of current user
+        Route::post('logout', [LogoutController::class, 'index']); // Logs User out
+    });
 });
 
-Route::get('file/{id}', [FileAccessController::class, 'index']);
-Route::get('tracks', [TrackController::class, 'index']);
-Route::get('tracks/search', [TrackController::class, 'show']);
-Route::post('login', [LoginController::class, 'index']);
-Route::post('refresh', [RefreshController::class, 'index']);
-Route::post('register', [RegisterController::class, 'index']);
+Route::prefix('/tracks')->group(function () {
+    Route::get('/', [TrackController::class, 'index']); // Get all tracks
+    Route::get('search', [TrackController::class, 'show']); // Gets list of matched tracks
+
+    Route::middleware('auth:api')->group(function () {
+        Route::post('/', [UploadController::class, 'store']); // Uploads one track
+        Route::delete('/', [DeleteController::class, 'destroy']); // Deletes one track
+    });
+});
+
+Route::prefix('/playlists')->group(function () {
+    Route::get('/', []);
+    Route::get('tracks', []);
+});
+
+Route::middleware('auth:api')->group(function () {
+
+    Route::prefix('/users')->group(function () {
+        Route::patch('role', [UpdateRoleController::class, 'update']); // Updates User`s role
+    });
+
+    Route::prefix('/playlists/me')->group(function () {
+        Route::get('/', [PlaylistController::class, 'index']); // Show list of User`s playlists
+        Route::post('/', [CreatePlaylistController::class, 'store']); // Creates playlist
+        Route::delete('/', [DeletePlaylistController::class, 'destroy']); // Deletes playlist
+
+        Route::prefix('/tracks')->group(function () {
+            Route::get('file/{id}', [FileAccessController::class, 'index']); // Gets one track by id
+
+            Route::get('/', [PlaylistController::class, 'show']); // Show list of User`s tracks in playlist
+            Route::post('/', [UpdatePlaylistController::class, 'store']); // Adds new track to playlist
+            Route::delete('/', [UpdatePlaylistController::class, 'destroy']); // Removes one track from playlist
+        });
+    });
+});
+
+
+
+// Route::middleware('auth:api')->group(function () {
+//     Route::get('profile', [ProfileController::class, 'show']); // Shows profile of current user
+//     Route::post('logout', [LogoutController::class, 'index']); // Logs User out
+//     Route::post('upload', [UploadController::class, 'store']); // Uploads one track
+//     Route::post('role', [UpdateRoleController::class, 'update']); // Updates User`s role
+//     Route::post('tracks/delete', [DeleteController::class, 'destroy']); // Deletes one track
+
+//     Route::get('playlists', [PlaylistController::class, 'index']); // Show list of User`s playlists
+//     Route::get('playlist/track', [PlaylistController::class, 'show']); // Show list of User`s tracks in playlist
+//     Route::post('playlist', [CreatePlaylistController::class, 'store']); // Creates playlist
+//     Route::post('playlist/add', [UpdatePlaylistController::class, 'store']); // Adds new track to playlist
+//     Route::post('playlist/remove', [UpdatePlaylistController::class, 'destroy']); // Removes one track from playlist
+//     Route::post('playlist/delete', [DeletePlaylistController::class, 'destroy']); // Deletes playlist
+// });
+
+// Route::get('file/{id}', [FileAccessController::class, 'index']); // Gets one track by id
+// Route::get('tracks', [TrackController::class, 'index']); // Get all tracks
+// Route::get('tracks/search', [TrackController::class, 'show']); // Gets list of matched tracks
