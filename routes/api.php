8ee005952fe4af93152c7eae04baa9ee844eafec
TheLastCookie-404 <se660745@gmail.com>
2026-05-25 16:52:22 +0500

    fix: api routes

--- routes/api.php
+++ routes/api.php
@@ -7,20 +7,20 @@ use App\Http\Controllers\Api\Playlist\PlaylistController;
 use App\Http\Controllers\Api\Playlist\PlaylistTrackController;
 use App\Http\Controllers\Api\Auth\ 
 {
-    ConfirmationController,
     RegisterController,
     LoginController,
     RefreshController,
     ProfileController,
     LogoutController,
     RoleController,
+    SendCodeController,
 };
 
 Route::prefix('/auth')->group(function () {
     Route::post('register', RegisterController::class); // Registers new User
     Route::post('login', LoginController::class); // Logs User in
     Route::put('refresh', RefreshController::class); // Refreshes User auth token
-    Route::post('confirm', ConfirmationController::class);
+    Route::post('confirm', SendCodeController::class);
 
     Route::middleware('auth:api')->group(function () {
         Route::get('profile', ProfileController::class); // Shows profile of current user
